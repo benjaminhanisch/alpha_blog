a05fad86511edb6d6027c42c3e5ecb37e6c361a8
Ben Hanisch <benjamin.hanisch@gmail.com>
2020-02-25 08:53:40 -0600

    add log in when signin up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Super Brand Blog #{@user.username}"
-      redirect_to users_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -27,7 +28,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       flash[:success] = "You account was updated successfully"
-      redirect_to users_path
+      redirect_to user_path
     else
       render 'edit'
     end
